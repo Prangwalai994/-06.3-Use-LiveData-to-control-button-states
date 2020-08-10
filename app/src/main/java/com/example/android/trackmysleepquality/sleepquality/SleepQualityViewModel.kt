92849bd40b0a5216262936c834952cc4013e9582
Wae <pangwalai.li@rmuti.ac.th>
2020-08-11 01:00:29 +0700

    6.3-4-Step 1: Create a ViewModel and a ViewModelFactory

--- app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
@@ -20,28 +20,41 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.*
 
 class SleepQualityViewModel(
 
 
         private val sleepNightKey: Long = 0L,
         val database: SleepDatabaseDao) : ViewModel() {
-        override fun onCleared() {
+    private val viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    override fun onCleared() {
         super.onCleared()
         viewModelJob.cancel()
     }
-}
+    private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
 
-        private val viewModelJob = Job()
-        private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+    val navigateToSleepTracker: LiveData<Boolean?>
+        get() = _navigateToSleepTracker
 
-        private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
-        val navigateToSleepTracker: LiveData<Boolean?>
-             get() = _navigateToSleepTracker
-
-        fun doneNavigating() {
+    fun doneNavigating() {
         _navigateToSleepTracker.value = null
+    }fun onSetSleepQuality(quality: Int) {
+        uiScope.launch {
+            // IO is a thread pool for running operations that access the disk, such as
+            // our Room database.
+            withContext(Dispatchers.IO) {
+                val tonight = database.get(sleepNightKey) ?: return@withContext
+                tonight.sleepQuality = quality
+                database.update(tonight)
+            }
+
+            // Setting this state variable to true will alert the observer and trigger navigation.
+            _navigateToSleepTracker.value = true
+        }
+    }
 }
+
+
